39ea31d2b737f00e37cdc17346b175f07e268fec
downthecrop <bryce@mathx.ca>
2023-01-05 20:16:23 -0800

    New Skyline/VueJS Site

---
+++ src/components/Navbar.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <nav class="dark:bg-darkBackground">
+      <div class="px-5 mx-auto max-w-6xl mx-auto max-w-page">
+        <div class="flex items-center justify-between h-20">
+          <div class="flex items-center ">
+            <a class="flex-shrink-0" href="/">
+              <img class="w-10 h-10 rounded-full" :src="logo" alt="" />
+            </a>
+            <a class="flex-col justify-center font-rubik" href="/">
+              <h1 class="px-4 text-xl font-bold text-pink-400 rounded-md dark:text-white" href="/#">
+                downthecrop
+              </h1>
+              <h1 class="px-4 text-xs font-bold text-gray-500 uppercase rounded-md" href="/#">
+                world famous video gamer
+              </h1>
+            </a>
+          </div>
+          <div class="hidden md:flex md:flex-row md:grow font-roboto">
+            <div class="flex items-baseline ml-10 space-x-4">
+              <a v-for="(i, idx) in paths" :key="idx"
+                class="px-3 py-2 text-sm tracking-wide font-bold text-gray-500 transition-colors duration-200 uppercase rounded-md dark:text-white hover:font-bold hover:text-pink-400 dark:hover:text-pink-400"
+                :href="i.path">
+                {{ i.name }}
+              </a>
+            </div>
+          </div>
+          <div class="hidden lg:block md:block">
+            <div class="flex items-center">
+              <a href="https://github.com/downthecrop"
+                class="p-1 text-gray-500 text-3xl transition-colors duration-200 rounded-full focus:outline-none hover:text-pink-400 dark:hover:text-pink-400 dark:text-white md:ml-2">
+                <font-awesome-icon icon="fa-brands fa-github" />
+              </a>
+              <a href="https://twitter.com/downthecrop"
+                class="p-1 text-gray-500 text-3xl transition-colors duration-200 rounded-full focus:outline-none hover:text-pink-400 dark:hover:text-pink-400 dark:text-white md:ml-2">
+                <font-awesome-icon icon="fa-brands fa-twitter" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <Slide class="absolute top-0 right" v-if="showHambuger" right>
+          <div class="block lg:hidden py-4 dark:bg-darkBackground">
+            <div class="block items-baseline">
+              <div v-for="(i, idx) in paths" :key="idx"
+                class="px-3 py-2 text-sm tracking-wide font-bold text-gray-500 transition-colors duration-200 uppercase rounded-md dark:text-white hover:font-bold hover:text-pink-400 dark:hover:text-pink-400">
+                <a :href="i.path">{{ i.name }}</a>
+              </div>
+              <a href="https://github.com/downthecrop"
+                class="p-1 text-gray-500 text-3xl transition-colors duration-200 rounded-full focus:outline-none hover:text-pink-400 dark:hover:text-pink-400 dark:text-white md:ml-2">
+                <font-awesome-icon icon="fa-brands fa-github" />
+              </a>
+              <a href="https://twitter.com/downthecrop"
+                class="p-1 text-gray-500 text-3xl transition-colors duration-200 rounded-full focus:outline-none hover:text-pink-400 dark:hover:text-pink-400 dark:text-white md:ml-2">
+                <font-awesome-icon icon="fa-brands fa-twitter" />
+              </a>
+            </div>
+          </div>
+        </Slide>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import { Slide } from 'vue3-burger-menu'  // import the CSS transitions you wish to use, in this case we are using `Slide`
+export default {
+  components: {
+    Slide // Register your component
+  },
+  data() {
+    return {
+      logo: "https://downthecrop.github.io/downthecrop.png",
+      showHambuger: false,
+      windowWidth: window.innerWidth,
+      paths: [
+        {
+          name: "Home",
+          path: "/#",
+        },
+        {
+          name: "About",
+          path: "/#about",
+        },
+        {
+          name: "Portfolio",
+          path: "/#portfolio",
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.onResize);
+    })
+    this.showHambuger = this.windowWidth <= 764
+  },
+
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize);
+  },
+
+  methods: {
+    onResize() {
+      this.windowWidth = window.innerWidth
+      this.showHambuger = this.windowWidth <= 764
+    }
+  }
+}
+</script>
+
+<style>
+.right {
+  z-index: 999;
+  right: 50px;
+}
+</style>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="w-full md:w-2/3 md:text-left text-center mx-auto pt-20">
+    <div v-for="project of projects">
+      <div class="mb-4">
+        <a target="_new" class="hover:underline" :href="project.url">
+        <h3 class="text-xl hover:underline font-semibold text-pink-400">{{project.name}}</h3>
+        </a>
+      </div>
+      <div class="mb-8 dark:text-white">
+        <p>
+          {{project.description}}
+          <br>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/*
+  TODO: Add sorting options based on date created and alphabetical order
+*/
+
+export default {
+data() {
+    return {
+      projects: [
+        {
+          name:"2009Scape-macOS-HD",
+          url: "https://github.com/downthecrop/2009Scape-macOS-HD",
+          description: "Exactly what it sounds like. Saradomin Launcher in a WineskinWrapper for HD/OpenGL on macOS.", 
+        },
+        {
+          name:"GIF Optimizer",
+          url: "https://github.com/downthecrop/GIF-Optimizer",
+          description: "Fast and customizable GIF compression! Electron app wrapper for FFMPEG.", 
+        },
+        {
+          name:"Minimal New Tab Extension",
+          url: "https://github.com/downthecrop/minimal-new-tab",
+          description: "I really don't like google doodles on my newtab pages. Unfortunatly due to some chrome premission settings the chrome://newtab page is unable to be hooked. And thus we can't just block doodle requests. This is an all in one drop in replacement for the Chrome new tab.", 
+        },
+        {
+          name:"Minimal Scrollbar",
+          url: "https://github.com/downthecrop/minimal-new-tab",
+          description: "Chrome improvement project. Makes the overflow scrollbar on pages consistent and minimal. All CSS. NO JavaScript. Featured by the Chromestore!", 
+        },
+        {
+          name:"RuneLite But You Can Save Your Password (BYCSYP)",
+          url: "https://github.com/downthecrop/runelite-but-you-can-save-your-password",
+          description: "RuneLite is a free, open source OldSchool RuneScape client... that didn't let you save your password and now you can!", 
+        },
+        {
+          name:"checkra1n TWRP",
+          url: "https://github.com/downthecrop/checkra1n-twrp",
+          description: "Automate TWRP commands to launch checkra1n on arm/arm64 Android", 
+        },
+        {
+          name:"checkra1n-linux minimal Live USB",
+          url: "https://github.com/downthecrop/checkra1n-linux",
+          description: "Portable minimal boot environment for use with checkra1n. This respin of Minimal Linux Live provides everything you need to run checkra1n for iOS/tvOS in under 70MB.", 
+        },
+        {
+          name:"OPGG Clone for WeGame/Tencent Chinese/CN Servers",
+          url: "https://github.com/downthecrop/wegame-tencent-china-opgg",
+          description: "Pregame lobby Multisearch, Game Details and Profile Statistics from WeGame/Tencent League of Legends LoL API", 
+        },
+        {
+          name:"Better Better Destiny.gg (BBDGG)",
+          url: "https://github.com/downthecrop/bbdgg",
+          description: "An unofficial browser extension to add unofficial functionality to the destiny.gg chat.", 
+        },
+        {
+          name:"OSRS Jad Simulator",
+          url: "https://github.com/downthecrop/JS-OSRS-Jad-Simulator",
+          description: "Asset swapped RS3 Jad Simulator for Old School Runescape", 
+        },
+        {
+          name:"Adobe Photoshop CS2 Patcher",
+          url: "https://github.com/downthecrop/CS2-Patcher",
+          description: "Asset swapped RS3 Jad Simulator for Old School Runescape", 
+        },
+        {
+          name:"Audio Remuxer",
+          url: "https://github.com/downthecrop/audio-remuxer",
+          description: "FFmpeg audio channel remuxer for use with multitrack recordings from OBS", 
+        },
+        {
+          name:"TwitchVOD",
+          url: "https://github.com/downthecrop/TwitchVOD",
+          description: "Keep your past broadcasts safe from being muted. Private remote VOD recordings.", 
+        },
+        {
+          name:"2009Scape Drop Table Tool",
+          url: "https://github.com/2009scape/2009scape.github.io/pull/173",
+          description: "Search which NPCs drop which items and their rarities.", 
+        },
+
+    ]
+    };
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
